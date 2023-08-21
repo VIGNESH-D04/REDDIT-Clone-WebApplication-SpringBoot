db80686540deb4b872ec5c2d17167de0e0b6750d
VIGNESH D <107434564+VIGNESH-D04@users.noreply.github.com>
2023-08-21 14:24:24 +0530

    Add files via upload

---
+++ src/main/java/com/reddit/RedditCloneApplication.java
@@ -0,0 +1,18 @@
+package com.reddit;
+
+import com.reddit.config.OpenAPIConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+@Import(OpenAPIConfiguration.class)
+public class RedditCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditCloneApplication.class, args);
+	}
+
+}
